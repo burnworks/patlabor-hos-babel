e2a7dd8314ea574352d7b130e8199e2cd5493149
burnworks <6436833+burnworks@users.noreply.github.com>
2024-08-15 23:02:13 +0900

    add GitHub Actions

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,8 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
+    site: 'https://burnworks.github.io',
+    base: '/patlabor-hos-babel',
+    trailingSlash: 'always',
     integrations: [tailwind()],
 });
